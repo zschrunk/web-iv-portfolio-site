49ec7fbee604548acce1f3150ede4d7f89706e34
zschrunk <dc:54:88:8d:7b:da:65:1a:2f:3b:fa:1a:c1:6a:18:89>
2015-05-14 15:52:34 -0500

    Relinked style sheets and scripts
    Fixed links and all that stuff for the index page. Also got a nav in
    progress for mobile hamburger

---
+++ scripts/custom.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+
+jQuery(document).ready(function($){
+	var navigation = $('#navigation'),
+	main = navigation.find('ul.menu');
+	$('.nav-trigger').on('click', function(){
+		$(this).toggleClass('open');
+		main.off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend').toggleClass('is-visible');
+	});
+});
