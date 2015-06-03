0b684b736bba0967150fb015eb3bcaaa2ee96bf4
zschrunk <dc:54:88:8d:7b:da:65:1a:2f:3b:fa:1a:c1:6a:18:89>
2015-06-03 12:23:33 -0500

    Added more photos
    Added and placed more photos
    Styled thanks page

--- contact-thanks.php
+++ contact-thanks.php
@@ -1,12 +1,10 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
 
 <head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Zak&rsquo;s Design</title>
   <script src="scripts/jquery-2.1.4.min.js"></script>
   <link rel="stylesheet" href="styles/foundation.css">
-  <link rel="stylesheet" href="styles/custom.css" />
+  <link rel="stylesheet" href="styles/custom.css"/>
   <script src="scripts/vendor/modernizr.js"></script>
   <script src="scripts/custom.js"></script>
   <script src="//use.typekit.net/zym7kfz.js"></script>
@@ -23,32 +21,25 @@
       <nav id="navigation">
         <div class="nav-trigger">Menu<span></span></div>
         <ul class="menu">
-          <li><a href="../index.html#home" title="Link">Home</a></li>
-          <li><a href="../index.html#work" title="Link">Work</a></li>
-          <li><a href="../index.html#about" title="Link">About</a></li>
-          <li><a href="../index.html#contact" title="Link">Contact</a></li>
+          <li><a href="index.html#home" title="Link">Home</a></li>
+          <li><a href="index.html#work" title="Link">Work</a></li>
+          <li><a href="index.html#about" title="Link">About</a></li>
+          <li><a href="index.html#contact" title="Link">Contact</a></li>
         </ul>
       </nav>
     </div>
   </div>
+  
+  <div class="triangles">
+    <img data-interchange="[images/triangle-medium-thanks.svg, (medium)], [images/triangle-large-thanks.svg, (large)]" alt="">
+  </div>
 
-	<div id="page-wrap">
-
-		<img src="images/title.gif" alt="A Nice & Simple Contact Form" />
-		<p>By <a href="http://css-tricks.com">CSS-Tricks</a></p>
-			
-			
-		<br /><br />
-		
-			
-		<h1>Your message has been sent!</h1><br />
-		
-		<a href="index.html">Back to Contact Form</a>
-	
-	</div>  
+  <div class="small-6 small-offset-3 text-center columns thanks">
+    <h2>Your message has been sent!</h2>
+      <a href="index.html#contact">Back to Contact Form</a>	
+  </div>  
   <script src="scripts/vendor/jquery.js"></script>
   <script src="scripts/foundation.min.js"></script>
-  <script src="scripts/foundation/foundation.interchange.js"></script>
   <script>
     $(document).foundation();
   </script>
