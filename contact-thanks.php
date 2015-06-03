21bba4e4518cf547e20b2fcc75d7a2683b6679a2
zschrunk <dc:54:88:8d:7b:da:65:1a:2f:3b:fa:1a:c1:6a:18:89>
2015-06-02 23:51:12 -0500

    Added more photos
    Added more photos but still need to link them in the code
    Fixed the size of subheads on mobile
    Centered social media icons
    Linked the images from home to their pages and then from one page to
    another
    Started on the thanks php page

--- contact-thanks.php
+++ contact-thanks.php
@@ -1,12 +1,36 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
 
 <head>
-	<title>A Nice & Simple Contact Form</title>
-	
-	<link rel="stylesheet" type="text/css" href="style.css" />
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Zak&rsquo;s Design</title>
+  <script src="scripts/jquery-2.1.4.min.js"></script>
+  <link rel="stylesheet" href="styles/foundation.css">
+  <link rel="stylesheet" href="styles/custom.css" />
+  <script src="scripts/vendor/modernizr.js"></script>
+  <script src="scripts/custom.js"></script>
+  <script src="//use.typekit.net/zym7kfz.js"></script>
+  <script>
+    try {
+      Typekit.load();
+    } catch (e) {}
+  </script>
 </head>
 
 <body>
+  <div class="large-12 columns">
+    <div class="container fixed">
+      <nav id="navigation">
+        <div class="nav-trigger">Menu<span></span></div>
+        <ul class="menu">
+          <li><a href="../index.html#home" title="Link">Home</a></li>
+          <li><a href="../index.html#work" title="Link">Work</a></li>
+          <li><a href="../index.html#about" title="Link">About</a></li>
+          <li><a href="../index.html#contact" title="Link">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  </div>
 
 	<div id="page-wrap">
 
@@ -19,17 +43,15 @@
 			
 		<h1>Your message has been sent!</h1><br />
 		
-		<p><a href="index.html">Back to Contact Form</a></p>
+		<a href="index.html">Back to Contact Form</a>
 	
-	</div>
-	
-	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
-	</script>
-	<script type="text/javascript">
-	_uacct = "UA-68528-29";
-	urchinTracker();
-	</script>
-
+	</div>  
+  <script src="scripts/vendor/jquery.js"></script>
+  <script src="scripts/foundation.min.js"></script>
+  <script src="scripts/foundation/foundation.interchange.js"></script>
+  <script>
+    $(document).foundation();
+  </script>
 </body>
 
 </html>
